ee08fe7085c466abcf38d845351bf2eef344ec7f
raul <ranufriev@ac-sw.com>
2020-11-16 12:23:29 +0700

    A little env improvements

--- bash/.bashrc
+++ bash/.bashrc
@@ -64,6 +64,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+export PATH="$PATH:/usr/local/bin"
+
 # App yarn global to path
 export PATH="$(yarn global bin):$PATH"
 
--- bash/.config/bash/functions.sh
+++ bash/.config/bash/functions.sh
@@ -2,44 +2,37 @@
 # Functions
 ####################################################################################################
 
-if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	C_RED='\[\033[1;31m\]'
-	C_GREEN='\[\033[1;32m\]'
+if [ -x /usr/bin/tput ] && tput setaf 1 >& /dev/null; then
+	C_RED='\[\033[01;31m\]'
+	C_GREEN='\[\033[01;32m\]'
 	C_BLUE='\[\033[01;34m\]'
-	C_YELLOW='\[\033[1;33m\]'
+	C_YELLOW='\[\033[01;33m\]'
 	C_WHITE='\[\033[00m\]'
 	C_PURPLE='\[\033[01;35m\]'
 fi
 
-git_branch() {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' | sed -e 's/\*\s\(.*\)$/\1/' | sed -e 's/(\(HEAD\)\s.*\s\(.*\))/\1 -> \2/' | sed -e 's/.*/ \0/'
-}
+# git_branch() {
+	# git branch 2> /dev/null | sed -e '/^[^*]/d' | sed -e 's/\*\s\(.*\)$/\1/' | sed -e 's/(\(HEAD\)\s.*\s\(.*\))/\1 -> \2/' | sed -e 's/.*/ \0/'
+# }
 
+# git_check_index() {
+	# local c=$1
+	# if git log >& /dev/null; then
+		# local index=$(git status --short | grep -E '^M' | wc --lines)
+		# local noindex=$(git status --short | grep -E '^\sM' | wc --lines)
+		# local untracked=$(git status --short | grep -E '^\?\?' | wc --lines)
+		# echo "$c($C_GREEN${index}$c|$C_RED${noindex}$c|$C_RED${untracked}$c) "
+	# fi
+# }
+
+_GIT_PS1="\$(__git_ps1 ' %s') "
+
+# PS1='${debian_chroot:+($debian_chroot)}'
 PS1="${C_GREEN}\u"
 PS1+="@\h "
 PS1+="${C_BLUE}\w"
-PS1+="${C_PURPLE}\$(git_branch) "
-PS1+="${C_WHITE}\$ ${C_WHITE}"
+PS1+="${C_PURPLE}${_GIT_PS1}"
+PS1+="${C_WHITE}\$ "
 
 export PS1
 
-# Work
-# TODO: rewrite this
-edit-ds-conf() {
-	filename="/etc/nginx/sites-available/webdispatch.conf"
-	md5f1=$(md5sum "$filename" | cut -d' ' -f1)
-	sudo $EDITOR "$filename"
-
-	md5f2=$(md5sum "$filename" | cut -d' ' -f1)
-
-	if [ "$md5f1" != "$md5f2" ]; then
-		echo -e "\nDispatch RPC has been changed!\n"
-		echo -e "${C_YELLOW}Waiting for nginx restart...${C_WHITE}"
-		if sudo service nginx restart ; then
-			echo -e "${C_GREEN}Nginx restarted success!${C_WHITE}\n"
-		else
-			echo -e "${C_RED}Cannot restart${C_WHITE}Nginx ${C_RED}service${C_WHITE}"
-		fi
-	fi
-
-}
